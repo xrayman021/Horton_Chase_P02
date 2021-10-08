7e88112d90f535b56ce73ae6c04b46d7bd2bf924
Chase's Hub <chaseghorton99@gmail.com>
2021-10-08 14:43:16 -0500

    Coded EnemyDesignerWindow script further

--- Assets/Editor/EnemyDesignerWindow.cs
+++ Assets/Editor/EnemyDesignerWindow.cs
@@ -5,11 +5,77 @@ using UnityEditor; //Won't be in final build
 
 public class EnemyDesignerWindow : EditorWindow
 {
+    Texture2D headerSectionTexture;
+    Texture2D mageSectionTexture;
+    Texture2D warriorSectionTexture;
+    Texture2D rogueSectionTexture;
+
+    Color headerSectionColor = new Color(13f/255f, 32f/255f, 44f/255f, 1f);
+
+    Rect headerSection;
+    Rect mageSection;
+    Rect warriorSection;
+    Rect rogueSection;
+
     [MenuItem("Window/Enemy Designer")]
     static void OpenWindow()
     {
         EnemyDesignerWindow window = (EnemyDesignerWindow)GetWindow(typeof(EnemyDesignerWindow));
         window.minSize = new Vector2(600, 300);
         window.Show();
+    }
+
+    //Similar to start() or awake()
+    void OnEnable()
+    {
+        InitTextures();
+    }
+
+    //initialize Texture 2D values
+    void InitTextures()
+    {
+        headerSectionTexture = new Texture2D(1, 1);
+        headerSectionTexture.SetPixel(0, 0, headerSectionColor);
+        headerSectionTexture.Apply();
+    }
+
+    void OnGUI()
+    {
+        DrawLayouts();
+        DrawHeader();
+        DrawMageSettings();
+        DrawWarriorSettings();
+        DrawRogueSettings();
+
+    }
+
+    void DrawLayouts()
+    {
+        headerSection.x = 0;
+        headerSection.y = 0;
+        headerSection.width = Screen.width;
+        headerSection.height = 50;
+
+        GUI.DrawTexture(headerSection, headerSectionTexture);
+    }
+
+    void DrawHeader()
+    {
+
+    }
+
+    void DrawMageSettings()
+    {
+
+    }
+
+    void DrawWarriorSettings()
+    {
+
+    }
+
+    void DrawRogueSettings()
+    {
+
     }
 }
